bb635670e9ed737dd7e0c191680ef269e04c6b31
Bojan Sekuloski <79922210+Sekuloski@users.noreply.github.com>
2021-12-26 13:42:33 +0100

    Working app.
    Changed some file names, now the app works.

--- Домашна 3/app.py
+++ Домашна 3/app.py
@@ -4,6 +4,21 @@ import json
 app = Flask(__name__)
 app.debug = True
 
+class Place:
+    def __init__(self, lat, lon, name):
+        self.lat = lat
+        self.lon = lon
+        self.name = name
+
+    def get_name(self):
+        return self.name
+    
+    def get_location(self):
+        return "https://www.openstreetmap.org/export/embed.html?bbox=" + str(self.lon) + "%2C" + str(self.lat) + "%2C" + str(self.lon) + "%2C" + str(self.lat) + "&amp;layer=mapnik&amp;marker=" + str(self.lon) + "%2C" + str(self.lat) + ""
+
+    def get_larger_map(self):
+        return "https://www.openstreetmap.org/?mlat=" + str(self.lat) + "&amp;mlon=" + str(self.lon) + "#map=15/" + str(self.lat) + "/" + str(self.lon)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,31 +26,52 @@ def index():
 @app.route('/restaurant', methods=['GET'])
 def dropdown_restaurant():
     cities = get_cities('restaurant')
-    return render_template('selected_food_type.html', cities=cities)
+    return render_template('restaurant.html', cities=cities)
 
 @app.route('/fastfood', methods=['GET'])
 def dropdown_fastfood():
     cities = get_cities('fastfood')
-    return render_template('selected_food_type.html', cities=cities)
+    return render_template('fastfood.html', cities=cities)
 
-@app.route('/selected_city', methods=['GET'])
-def places():
-    places = get_places()
+@app.route('/restaurant/choices', methods=['GET'])
+def restaurant_places():
     city = request.args.get('city')
+    places = get_places('restaurant', city)
     print(city)
-    return render_template('selected_city.html')
+    return render_template('selected_city.html', places=places)
 
-def get_places(selection):
+@app.route('/fastfood/choices', methods=['GET'])
+def fastfood_places():
+    city = request.args.get('city')
+    places = get_places('fastfood', city)
+    print(city)
+    return render_template('selected_city.html', places=places)
+
+def get_places(selection, city):
     if selection == 'restaurant':
         with open('restaurant_places.json') as json_file:
-            data = json.load(json_file)
+            data = json.load(json_file)[city]
             places = []
+
+            for place in data:
+                lat = place['lat']
+                lon = place['lon']
+                name = place['tags']['name']
+                place = Place(lat, lon, name)
+                places.append(place)
 
         return places
     if selection == 'fastfood':
         with open('fast_food_places.json') as json_file:
-            data = json.load(json_file)
+            data = json.load(json_file)[city]
             places = []
+
+            for place in data:
+                lat = place['lat']
+                lon = place['lon']
+                name = place['tags']['name']
+                place = Place(lat, lon, name)
+                places.append(place)
 
         return places
 
