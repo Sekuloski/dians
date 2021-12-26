e0187614f73f7144aa80f1d7e643015da649c5ed
Bojan Sekuloski <79922210+Sekuloski@users.noreply.github.com>
2021-12-26 11:53:00 +0100

    Updated HTML, CSS and JS files to work with Flask.

--- Домашна 3/app.py
+++ Домашна 3/app.py
@@ -8,19 +8,33 @@ app.debug = True
 def index():
     return render_template('index.html')
 
-@app.route('/cities', methods=['GET'])
-def dropdown():
+@app.route('/restaurant', methods=['GET'])
+def dropdown_restaurant():
     cities = get_cities('restaurant')
-    return render_template('index.html', cities=cities)
+    return render_template('selected_food_type.html', cities=cities)
+
+@app.route('/fastfood', methods=['GET'])
+def dropdown_fastfood():
+    cities = get_cities('fastfood')
+    return render_template('selected_food_type.html', cities=cities)
 
 def get_cities(selection):
-    with open('restaurant_places.json') as json_file:
-        data = json.load(json_file)
-        cities_sk = []
+    if selection == 'restaurant':
+        with open('restaurant_places.json') as json_file:
+            data = json.load(json_file)
+            cities_sk = []
 
-        for key in data.keys():
-            cities_sk.append(key)
-    return cities_sk
+            for key in data.keys():
+                cities_sk.append(key)
+        return cities_sk
+    if selection == 'fastfood':
+        with open('fast_food_places.json') as json_file:
+            data = json.load(json_file)
+            cities_sk = []
+
+            for key in data.keys():
+                cities_sk.append(key)
+        return cities_sk
 
 if __name__ == "__main__":
     app.run()
