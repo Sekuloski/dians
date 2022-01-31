859523ab6f67877822acc3843db8ded373f27ef4
Bojan Sekuloski <79922210+Sekuloski@users.noreply.github.com>
2022-01-31 18:21:35 +0100

    Refactoring

--- Домашна 3 и 4/Data collection/data_collection.py
+++ Домашна 3 и 4/Data collection/data_collection.py
@@ -1,9 +1,39 @@
 import requests
 import json
 from geopy.geocoders import Nominatim
+from flask import Flask
 
-if __name__ == '__main__':
+app = Flask(__name__)
+app.debug = True
 
+
+@app.route('/')
+def index():
+    return "Hello"
+
+
+@app.route('/restaurants', methods=['GET'])
+def restaurants():
+    with open('json/restaurant_places.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+
+@app.route('/fast_food', methods=['GET'])
+def restaurants():
+    with open('json/fast_food_places.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+
+@app.route('/update', methods=['GET'])
+def update():
+    get_fast_food_places()
+    get_restaurant_places()
+
+
+def get_fast_food_places():
+    global overpass_url, overpass_query, response, fast_food_places
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = """
     [out:json];
@@ -16,42 +46,7 @@ if __name__ == '__main__':
     """
     response = requests.get(overpass_url, params={'data': overpass_query})
     fast_food_places = response.json()['elements']
-
-    overpass_url = "http://overpass-api.de/api/interpreter"
-    overpass_query = """
-    [out:json];
-    area["ISO3166-1"="MK"][admin_level=2];
-    (node["amenity"="restaurant"](area);
-     way["amenity"="restaurant"](area);
-     rel["amenity"="restaurant"](area);
-    );
-    out center;
-    """
-    response = requests.get(overpass_url, params={'data': overpass_query})
-    restaurants = response.json()['elements']
-
-    restaurant_dict = {}
     fast_food_dict = {}
-
-    for restaurant in restaurants:
-        tags = restaurant['tags']
-        if 'name' in tags:
-            locator = Nominatim(user_agent='myGeocoder')
-            coordinates = str(restaurant['lat']) + ', ' + str(restaurant['lon'])
-            location = locator.reverse(coordinates)
-            address = location.raw['address']
-            if 'city' in address:
-                city = address['city']
-                if city in restaurant_dict:
-                    restaurant_dict[city].append(restaurant)
-                else:
-                    restaurant_dict[city] = [restaurant]
-            elif 'village' in address:
-                village = address['village']
-                if village in restaurant_dict:
-                    restaurant_dict[village].append(restaurant)
-                else:
-                    restaurant_dict[village] = [restaurant]
 
     for fast_food in fast_food_places:
         tags = fast_food['tags']
@@ -74,11 +69,50 @@ if __name__ == '__main__':
                     fast_food_dict[village] = [fast_food]
 
     json_file = json.dumps(fast_food_dict)
-    f = open("fast_food_places.json", "w")
+    f = open("json/fast_food_places.json", "w")
     f.write(json_file)
     f.close()
 
+
+def get_restaurant_places():
+    global overpass_url, overpass_query, response
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = """
+    [out:json];
+    area["ISO3166-1"="MK"][admin_level=2];
+    (node["amenity"="restaurant"](area);
+     way["amenity"="restaurant"](area);
+     rel["amenity"="restaurant"](area);
+    );
+    out center;
+    """
+    response = requests.get(overpass_url, params={'data': overpass_query})
+    restaurants = response.json()['elements']
+    restaurant_dict = {}
+    for restaurant in restaurants:
+        tags = restaurant['tags']
+        if 'name' in tags:
+            locator = Nominatim(user_agent='myGeocoder')
+            coordinates = str(restaurant['lat']) + ', ' + str(restaurant['lon'])
+            location = locator.reverse(coordinates)
+            address = location.raw['address']
+            if 'city' in address:
+                city = address['city']
+                if city in restaurant_dict:
+                    restaurant_dict[city].append(restaurant)
+                else:
+                    restaurant_dict[city] = [restaurant]
+            elif 'village' in address:
+                village = address['village']
+                if village in restaurant_dict:
+                    restaurant_dict[village].append(restaurant)
+                else:
+                    restaurant_dict[village] = [restaurant]
     json_file = json.dumps(restaurant_dict)
     f = open("restaurant_places.json", "w")
     f.write(json_file)
     f.close()
+
+
+if __name__ == '__main__':
+    app.run()
